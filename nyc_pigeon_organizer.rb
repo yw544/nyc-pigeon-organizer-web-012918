8b6cecaee716f7c82a45d5caf85206693e45c500
York Wang <yw544@nyu.edu>
2018-01-23 08:15:22 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,12 +6,12 @@ def nyc_pigeon_organizer(data)
       names.each do |name|
         if open_hash.include?(name)
           if open_hash[name].include?(type)
-            open_hash[name][type].push(char.to_s)
+            open_hash[name][type].push(attributes.to_s)
           else
-            open_hash[name][type] = [char.to_s]
+            open_hash[name][type] = [attributes.to_s]
 end
 else
-  open_hash[name] = {type => [char.to_s]}
+  open_hash[name] = {type => [attributes.to_s]}
 end
 end
 end
